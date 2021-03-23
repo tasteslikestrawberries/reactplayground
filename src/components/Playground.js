a348bb28d60a932c4591aa64d8d2af816bf63841
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-03-23 21:28:24 +0100

    up

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,10 +1,41 @@
 import React from 'react';
 
+/*
+let arr = ['Rijeka', 'Zagreb', 'Pula', 'Split' ];
+arr.push('Dubrovnik');
+console.log(arr);
+
+
+
+let x = () => {
+    let anja = 5+2;
+    let lily = 10+3;
+    if (anja >= 7 && lily === 13) {
+    alert('Cool');
+}
+
+    else {
+        alert('Sipak');
+    }
+  
+};
+
+x();
+console.log(x);
+*/
+
+
+
+
 const Playground = () => {
     return(
 <>
-Bubi
-</>)
+
+<div className='bubi'></div>
+<div className='buba'></div>
+</>
+
+);
 };
 
 export default Playground
