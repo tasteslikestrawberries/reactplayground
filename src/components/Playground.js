c1c80ed3d148471dace26ea9184f1e9281de818a
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-03-24 22:12:39 +0100

    up

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -26,13 +26,21 @@ console.log(x);
 
 
 
-
 const Playground = () => {
+
+function clickHandler(event) {
+    console.log('Button Clicked');
+ };
+
     return(
 <>
 
-<div className='bubi'></div>
-<div className='buba'></div>
+<div className='container1'>Blue Container <br/><br/>
+
+    <button className='changebtn' onClick={clickHandler}>Click Me</button>
+
+</div>
+<div className='container2'>Pink Container</div>
 </>
 
 );
